9dece7522eaa81e1acd106f44829c843ea607c4a
yh <15248004@qq.com>
2014-12-29 22:33:29 +0800

    add player unit test

--- src/model/player.js
+++ src/model/player.js
@@ -7,4 +7,20 @@ function Player(name, occupation, hp, attack, weapon, armor) {
   this.armor = armor||null;
 }
 
+Player.prototype.getWeaponName = function() {
+  return this.weapon.name;
+};
+
+Player.prototype.getWeaponAttack = function() {
+  return this.weapon.attack;
+};
+
+Player.prototype.getArmorName = function() {
+  return this.armor.name;
+};
+
+Player.prototype.getArmorDef = function() {
+  return this.armor.def;
+};
+
 module.exports = Player;
