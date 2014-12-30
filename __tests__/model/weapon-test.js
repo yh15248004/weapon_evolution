c8662842108644034c28a7cdf7c29886e1a0c979
yh <15248004@qq.com>
2014-12-30 21:35:07 +0800

    add isTigger() unit test

--- __tests__/model/weapon-test.js
+++ __tests__/model/weapon-test.js
@@ -1,15 +1,19 @@
 jest.dontMock('../../src/model/weapon');
+jest.dontMock('../../src/model/effect');
+jest.dontMock('lodash');
 
+var Effect = require('../../src/model/effect');
+var Weapon = require('../../src/model/weapon');
 describe('Weapon', function() {
-  var Weapon;
   var weapon;
+
   beforeEach(function() {
-    Weapon = require('../../src/model/weapon');
-    weapon = new Weapon('xuanyuan', 20);
+    weapon = new Weapon('xuanyuan', 20, new Effect('致命一击', 1));
   });
 
   describe('#getWeaponName()', function() {
     it('should return correct name', function() {
+
       var result = weapon.getWeaponName();
 
       expect(result).toBe('xuanyuan');
@@ -23,6 +27,17 @@ describe('Weapon', function() {
       var result = weapon.getWeaponAttack();
 
       expect(result).toBe(20);
+    });
+
+  });
+
+  describe('#isTrigger()', function() {
+
+    it('should return correct boolean', function() {
+
+      var result = weapon.isTrigger();
+
+      expect(result).toBe(true);
     });
 
   });
