c64aea8a52d37c12a826688fa857b478e26b409b
yh <15248004@qq.com>
2014-12-31 12:41:16 +0800

    add EffectHouse

---
+++ src/model/effect-house.js
@@ -0,0 +1,5 @@
+function EffectHouse(isTrigger, message, damange) {
+    this.isTrigger = isTrigger;
+    this.message = message;
+    this.damange = damange;
+};
--- src/model/effect.js
+++ src/model/effect.js
@@ -4,4 +4,17 @@ function Effect(name, probability, attack, times) {
   this.attack = attack || 0;
   this.times = times || 0;
 }
+
+Effect.prototype.getEffectHouse = function(player) {
+  var isTrigger = false;
+  var message = '';
+  if (this.probability * 100 > _.random(0, 99) && this.name === '致命一击') {
+    isTrigger = true;
+    message = player + '发动了' + this.name + ',';
+    damange = damange * 3;
+  }
+  return new EffectHouse(isTrigger, message, damange);
+};
+
 module.exports = Effect;
+
